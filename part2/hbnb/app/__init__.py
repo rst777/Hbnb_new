ca59e1ed5f611119ab054885ac05396c87a606d7
Your Name <you@example.com>
2024-10-23 16:50:56 +0200

    hbnb ari

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -7,7 +7,7 @@ from app.services.facade import HBnBFacade
 def create_app():
     """Create Flask application"""
     app = Flask(__name__)
-    
+
     # Create API
     api = Api(
         app,
@@ -15,17 +15,18 @@ def create_app():
         title='HBnB API',
         description='HBnB REST API'
     )
-    
+
     # Initialize Facade
     app.facade = HBnBFacade()  # Cette ligne manquait
-    
+
     # Register namespaces
-    from app.api.v1.place import api as place_api
-    from app.api.v1.review import api as review_api
-    from app.api.v1.amenity import api as amenity_api
-    
-    api.add_namespace(place_api, path='/api/v1/place')
-    api.add_namespace(review_api, path='/api/v1/review')
-    api.add_namespace(amenity_api, path='/api/v1/amenity')
-    
+    from app.api.v1.places import api as place_api
+    from app.api.v1.reviews import api as review_api
+    from app.api.v1.amenities import api as amenity_api
+    from app.api.v1.users import api as user_api
+
+    api.add_namespace(place_api, path='/api/v1/places')
+    api.add_namespace(review_api, path='/api/v1/reviews')
+    api.add_namespace(amenity_api, path='/api/v1/amenities')
+    api.add_namespace(user_api, path='/api/v1/users')
     return app
--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,9 +1,9 @@
 """API v1 endpoints"""
 
 from flask_restx import Api
-from app.api.v1.user import api as user_api
-from app.api.v1.place import api as place_api
-from app.api.v1.review import api as review_api
-from app.api.v1.amenity import api as amenity_api
+from app.api.v1.users import api as user_api
+from app.api.v1.places import api as place_api
+from app.api.v1.reviews import api as review_api
+from app.api.v1.amenities import api as amenity_api
 
 __all__ = ['place_api', 'review_api', 'amenity_api']
